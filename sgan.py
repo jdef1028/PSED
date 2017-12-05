95cdd50fa27e9fb98cd09966ab68a926b4b647e1
Xiaolin Li <xiaolinli@Xiaolins-MacBook-Pro-2.local>
2017-12-04 23:46:46 -0600

    first build of G&D

--- sgan.py
+++ sgan.py
@@ -1,4 +1,7 @@
 from __future__ import print_function
+from keras.models import Sequential
+from keras.layers import Conv2D, Conv2DTranspose, BatchNormalization, LeakyReLU
+from keras.initializers import Zeros, RandomNormal, RandomUniform
 
 # input tensor dimensions
 Z_l = 2
@@ -26,14 +29,96 @@ X_m = Z_m * (2**len(g_filter_depths))
 
 print("The dimension of the cropped image is: (" + str(X_l) + " x " + str(X_m) +")")
 
-
+#batch parameters
+batch_size = 64
 
 class SGAN(object):
 	def __init__(self):
-		pass
+		self._build_sgan()
 
 	def _build_sgan(self):
-		pass
+
+		# build generator
+		self.generator = Sequential()
+		for lv in xrange(len(g_filter_sizes)-1): # construction all layers in generator but not the last
+			if lv == 0:
+				self.generator.add(Conv2D(filters=g_filter_depths[lv],
+							 kernel_size=g_filter_sizes[lv],
+							 padding="same",
+							 activation='relu',
+							 dilation_rate=(2, 2),
+							 kernel_initializer=RandomNormal(stddev=0.02),
+							 bias_initializer=Zeros(),
+							 input_shape=(Z_d, Z_l, Z_m)
+							 )
+					 )
+			else:
+				self.generator.add(Conv2D(filters=g_filter_depths[lv],
+							 kernel_size=g_filter_sizes[lv],
+							 padding="same",
+							 activation='relu',
+							 dilation_rate=(2, 2),
+							 kernel_initializer=RandomNormal(stddev=0.02),
+							 bias_initializer=Zeros(),
+							 )
+					 )
+			self.generator.add(BatchNormalization(beta_initializer='zeros',
+										 gamma_initializer='ones'))
+
+		# add the last layer of the generator
+		self.generator.add(Conv2D(filters=g_filter_depths[lv],
+							 kernel_size=g_filter_sizes[lv],
+							 padding="same",
+							 activation='tanh',
+							 dilation_rate=(2, 2),
+							 kernel_initializer=RandomNormal(stddev=0.02),
+							 bias_initializer=Zeros()
+							 )
+				 )
+		print("=== Generator built ====")
+
+		# build discriminator
+		self.discrimitor = Sequential()
+		for lv in xrange(len(d_filter_sizes)-1):
+			if lv == 0:
+				self.discrimitor.add(Conv2DTranspose(filters=g_filter_depths[lv],
+							 kernel_size=g_filter_sizes[lv],
+							 padding="same",
+							 dilation_rate=(2, 2),
+							 kernel_initializer=RandomNormal(stddev=0.02),
+							 bias_initializer=Zeros(),
+							 input_shape=(n_channel, X_l, X_m)
+							 )
+					 )
+			else:
+				self.discrimitor.add(Conv2DTranspose(filters=g_filter_depths[lv],
+							 kernel_size=g_filter_sizes[lv],
+							 padding="same",
+							 dilation_rate=(2, 2),
+							 kernel_initializer=RandomNormal(stddev=0.02),
+							 bias_initializer=Zeros(),
+							 )
+					 )
+				self.discrimitor.add(LeakyReLU(alpha=0.2))
+
+		self.discrimitor.add(Conv2DTranspose(filters=g_filter_depths[lv],
+							 kernel_size=g_filter_sizes[lv],
+							 padding="same",
+							 activation='tanh',
+							 dilation_rate=(2, 2),
+							 kernel_initializer=RandomNormal(stddev=0.02),
+							 bias_initializer=Zeros()
+							 )
+				 )
+
+		print("=== Discriminator built === ")
+
+
+
+Model = SGAN()
+
+
+
 
 
 
