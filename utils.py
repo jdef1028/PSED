2e5056d95f9f5b99ad9efb2cfd166405cabe258e
Xiaolin Li <xiaolinli@Xiaolins-MacBook-Pro-2.local>
2017-12-07 20:24:56 -0600

    update utils.py to fit the mat input format

--- utils.py
+++ utils.py
@@ -1,24 +1,26 @@
 # Utility functions for this project
 # Xiaolin Li & Zijiang Yang
-
+from __future__ import print_function
+import h5py
 import random
 import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
 def sample_cropped_img(img_collections, batch_size, l, m, n_channel, seed=999):
 	#randomly sample one image from image_collections
 	#then crop a lxm sub-image from this sampled image
 	random.seed(seed)
-	n_imgs = len(img_collections)
+	n_imgs = img_collections.shape[0]
 	img_batch = np.zeros((batch_size, l, m, n_channel))
 
 	for batch_idx in xrange(batch_size):
 
 		img_index = random.randint(0, len(img_collections)-1) # pick an image
-		img = img_collections[img_index, :, :, :]
+		img = img_collections[img_index, :]
+		img_size = int(sqrt(img.shape[0]))
+		img = np.reshape(img, (img_size, img_size))
 
-		if n_channel == 1:
-			L1, L2 = img.shape
-		elif n_channel == 3:
-			L1, L2, _ = img.shape
+		L1, L2 = img.shape
 	
 		assert L1 >= l
 		assert L2 >= m
@@ -26,11 +28,23 @@ def sample_cropped_img(img_collections, batch_size, l, m, n_channel, seed=999):
 		x1 = random.randint(0, L1 - l)
 		y1 = random.randint(0, L2 - m)
 
-		if len(img.shape) == 2:
-			cropped_img = img[x1: x1+l, y1: y1+m]
-		elif len(img.shape) == 3:
-			cropped_img = img[x1: x1+l, y1: y1+m, :]
 
-		img_batch[batch_idx, :, :, :] = cropped_img
+
+		cropped_img = img[x1: x1+l, y1: y1+m]
+		for i in xrange(n_channel):
+			img_batch[batch_idx, :, :, i] = cropped_img
 
 	return img_batch
+
+if __name__ == '__main__':
+
+	with h5py.File('./data/data.mat', 'r') as f:
+		img_collections = list(f['IMG'])
+	img_collections = np.array(img_collections)
+	img_collections = img_collections.T
+	print(img_collections.shape)
+	img_batch = sample_cropped_img(img_collections, 32, 50, 50, 3)
+	print(img_batch.shape)
+	temp = img_batch[1, :, :, :]
+	plt.imshow(temp)
+	plt.show()
